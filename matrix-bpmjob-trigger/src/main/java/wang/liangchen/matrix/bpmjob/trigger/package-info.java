317ce8cd68174aca1f005ce332a9be23f074321c
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2022-10-25 13:46:06 +0800

    注册节点 心跳 监视节点

---
+++ matrix-bpmjob-trigger/src/main/java/wang/liangchen/matrix/bpmjob/trigger/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * BoundedContext
+ * domain type: {@link wang.liangchen.matrix.framework.ddd.domain.DomainType}
+ */
+@BoundedContext(name = "trigger", domainType = DomainType.Core)
+package wang.liangchen.matrix.bpmjob.trigger;
+
+import wang.liangchen.matrix.framework.ddd.BoundedContext;
+import wang.liangchen.matrix.framework.ddd.domain.DomainType;
---
+++ matrix-bpmjob-trigger/src/main/java/wang/liangchen/matrix/bpmjob/trigger/southbound_acl/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Southbound
+ * Anti Corruption Layer
+ * port type: {@link wang.liangchen.matrix.framework.ddd.southbound_acl.PortType}
+ */
+package wang.liangchen.matrix.bpmjob.trigger.southbound_acl;
